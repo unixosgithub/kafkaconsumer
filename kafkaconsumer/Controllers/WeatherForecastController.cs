892de1960d8cfba39796af6c17b3b6a1ebf27bc5
Mohammed Ali <unixos@yahoo.com>
2023-08-20 05:05:30 -0500

    Update WeatherForecastController.cs

--- kafkaconsumer/Controllers/WeatherForecastController.cs
+++ kafkaconsumer/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace kafkaconsumer.Controllers
 {
     [ApiController]
-    [Route("[consumer]")]
+    [Route("/consumer")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
